b9365f1fb4835053fbc2aee94c2e1d738633ff75
ChanDru-Balu <prochandru@gmail.com>
2023-11-03 16:42:41 +0530

    Config Updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'grocery',
+  appId: 'chan.ecom.delivery',
+  appName: 'Grocery',
   webDir: 'www',
   server: {
     androidScheme: 'https'
